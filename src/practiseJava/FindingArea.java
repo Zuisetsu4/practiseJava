0c163ccc0058d7dd8007290482c715d6e720f752
Zuisetsu <3nextlvlplay3@gmail.com>
2023-01-08 19:03:07 +0300

    Initial commit

---
+++ src/practiseJava/FindingArea.java
@@ -0,0 +1,11 @@
+package practiseJava;
+
+public class FindingArea {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(areaSquare(a));
+    }
+    public static int areaSquare(int a){
+        return a * a;
+    }
+}
---
+++ src/practiseJava/Introduction.java
@@ -0,0 +1,7 @@
+package practiseJava;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.print("2.71 8281 8284 5904 5235 3602 8747 1352 6624 9775 7247 0936 9995 9574 9669 6762");
+    }
+}
